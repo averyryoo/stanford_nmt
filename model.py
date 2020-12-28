888a39a03d49a217519c1f8f75736731e5cb54b5
averyryoo <averyryoo@outlook.com>
2020-12-28 11:27:58 -0500

    added Attention

--- model.py
+++ model.py
@@ -37,3 +37,50 @@ class Encoder(tf.keras.Model):
 
     def initialize_cell_state(self):
         return tf.zeros((self.batch_size, self.enc_units))
+
+class Attention(tf.keras.Model):
+    def __init__(self, enc_units, method='concat'):
+        super(Attention, self).__init__()
+        self.method = method
+        self.W = tf.keras.layers.Dense(enc_units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, dec_h_t, enc_h_s):
+
+        if self.method == 'concat':
+            score = self.V(tf.nn.tanh(self.W_a(dec_h_t + enc_h_s)))
+        elif self.method == 'general':
+            score = tf.matmul(self.W(enc_h_s), dec_h_t, transpose_b=True)
+        elif self.method == 'dot':
+            score = tf.matmul(enc_h_s, dec_h_t, transpose_b=True) 
+
+        # a_t shape == (batch_size, seq_len, 1)
+        a_t = tf.nn.softmax(score, axis=1)
+
+        context_vector = tf.reduce_sum(a_t * enc_h_s, axis=1)
+
+        return context_vector
+
+class Decoder(tf.keras.Model):
+    def __init__(self, vocab_size, embed_dims, enc_units, batch_size, dropout):
+        super(Decoder, self).__init__()
+        
+        self.batch_size = batch_size
+
+
+        self.lstm_1 = tf.keras.layers.LSTM(
+            self.enc_units,
+            return_sequences = True,
+            return_state = True,
+            dropout = dropout,
+            recurrent_initializer = 'glorot_uniform'
+        )
+
+        self.lstm_2 = tf.keras.layers.LSTM(
+            self.enc_units,
+            return_sequences = True,
+            return_state = True,
+            dropout = dropout,
+            recurrent_initializer = 'glorot_uniform'
+        )
+    
