6ae20d28d844d66b0bbfa43961bea86b838543ce
averyryoo <averyryoo@outlook.com>
2021-01-05 11:32:37 -0500

    added data_gen function

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-import cv2
 import random
 import argparse
 import collections
@@ -11,18 +10,12 @@ import copy
 
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw, ImageColor, ImageFont
 
 import tensorflow as tf
-from tensorflow.keras.utils import multi_gpu_model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import nengo_dl
-import nengo
 import pandas as pd
 
 from utils import *
-
-
+from model import Encoder, Decoder, Attention
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -30,14 +23,37 @@ def parse_args():
     parser.add_argument('--train', type=bool)
     parser.add_argument('--test', type=bool)
 
+    parser.add_argument('--data_dir', type=str, default='data/train')
     parser.add_argument('--batch_size', type=int, default=1)
-
-
+    parser.add_argument('--input_lang', type=str, default='en')
+    parser.add_argument('--target_lang', type=str, default='vi')
+    parser.add_argument('--max_len', type=int, default=100)
+    parser.add_argument('--limit_size', type=int)
+    
     args = parser.parse_args()
     return args
 
-
-def
+def data_gen():
+    print('Loading input dataset')
+    tensor_input, tokenizer_input = load_dataset(
+        args.data_dir,
+        args.input_lang,
+        args.max_len,
+        args.limit_size
+        )
+    
+    print('Loading target dataset')
+    tensor_target, tokenizer_target = load_dataset(
+        args.data_dir,
+        args.target_lang,
+        args.max_len,
+        args.limit_size
+        )
+    
+    print('Dataset loading complete')
+    return tensor_input, tensor_target, tokenizer_input, tokenizer_target
 
 if __name__ == '__main__':
-    
+    args = parse_args()
+    tensor_input, tensor_target, tokenizer_input, tokenizer_target = data_gen()
+    print(len(tensor_input[0]), len(tensor_target[0]))
