b05a8aca1fadec63e3f0e30814c8afb145267eec
averyryoo <averyryoo@outlook.com>
2021-01-05 16:58:04 -0500

    added train dataset generation

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import collections
 import time
 from tqdm import tqdm
 import copy
+from sklearn.model_selection import train_test_split
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,8 +21,8 @@ from model import Encoder, Decoder, Attention
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--train', type=bool)
-    parser.add_argument('--test', type=bool)
+    parser.add_argument('--train', default=False, action='store_true')
+    parser.add_argument('--test', default=False, action='store_true')
 
     parser.add_argument('--data_dir', type=str, default='data/train')
     parser.add_argument('--batch_size', type=int, default=1)
@@ -29,7 +30,12 @@ def parse_args():
     parser.add_argument('--target_lang', type=str, default='vi')
     parser.add_argument('--max_len', type=int, default=100)
     parser.add_argument('--limit_size', type=int)
-    
+    parser.add_argument('--dev_split', type=float, default=0.1)
+    parser.add_argument('--epochs', type=float, default=12)
+
+    parser.add_argument('--embed_dims', type=float, default=500)
+    parser.add_argument('--enc_units', type=float, default=)
+
     args = parser.parse_args()
     return args
 
@@ -53,7 +59,20 @@ def data_gen():
     print('Dataset loading complete')
     return tensor_input, tensor_target, tokenizer_input, tokenizer_target
 
+def train(tensor_input, tensor_target, tokenizer_input, tokenizer_target):
+    train_input, val_input, train_target, val_target = train_test_split(
+        tensor_input,
+        tensor_target,
+        test_size = args.dev_split
+    )
+
+    dataset = tf.data.Dataset.from_tensor_slices((train_input,train_target))
+    dataset = dataset.shuffle(len(train_input))
+
+    print(dataset)
+
 if __name__ == '__main__':
     args = parse_args()
     tensor_input, tensor_target, tokenizer_input, tokenizer_target = data_gen()
-    print(len(tensor_input[0]), len(tensor_target[0]))
+    if args.train:
+        train(tensor_input, tensor_target, tokenizer_input, tokenizer_target)
